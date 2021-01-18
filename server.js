43e997cde49443111417b8ed874a473c2484cd51
gzamora <gzamora@cartfulsolutions.com>
2021-01-18 16:57:44 -0600

    New ENV variable to be read in runtime

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ function sleep(ms) {
 }
 
 async function main() {
-  while(true) {
+  while (true) {
     console.log('Containers rule!');
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
